fa1eaf16dc10057fbbf993b2c9407f3ca6845636
ChelseaFlores <47254947+ChelseaFlores@users.noreply.github.com>
2019-04-05 11:13:16 -0500

    Transaction Constructor
    Transaction class - addTransaction method needed.

---
+++ Saving Stacks/src/application/model/Transaction.java
@@ -0,0 +1,94 @@
+package application.model;
+
+public class Transaction {
+	
+	private int transId;
+	private String entryDate;
+	private String transDate;
+	private String name;
+	private String tag;
+	
+	public Transaction(int idNumber, String entryDate, String transDate, String name, String tag)
+	{
+		this.transId = idNumber;
+		this.entryDate = entryDate;
+		this.transDate = transDate;
+		this.name = name;
+		this.tag = tag;
+	}
+	
+	//TODO: add addTransaction function
+
+	/**
+	 * @return the entryDate
+	 */
+	public String getEntryDate() {
+		return entryDate;
+	}
+
+	/**
+	 * @param entryDate the entryDate to set
+	 */
+	public void setEntryDate(String entryDate) {
+		this.entryDate = entryDate;
+	}
+
+	/**
+	 * @return the transDate
+	 */
+	public String getTransDate() {
+		return transDate;
+	}
+
+	/**
+	 * @param transDate the transDate to set
+	 */
+	public void setTransDate(String transDate) {
+		this.transDate = transDate;
+	}
+
+	/**
+	 * @return the transId
+	 */
+	public int getTransId() {
+		return transId;
+	}
+
+	/**
+	 * @param transId the transId to set
+	 */
+	public void setTransId(int transId) {
+		this.transId = transId;
+	}
+
+
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the tag
+	 */
+	public String getTag() {
+		return tag;
+	}
+
+	/**
+	 * @param tag the tag to set
+	 */
+	public void setTag(String tag) {
+		this.tag = tag;
+	}
+
+}

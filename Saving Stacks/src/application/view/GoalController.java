635377b2db77f06555d243dfc7fd949c2c923ce5
ChelseaFlores <47254947+ChelseaFlores@users.noreply.github.com>
2019-04-06 14:04:11 -0500

    GoalController.java
    IN PROGRESS:  GoalController handles user input and works with the GoalSet.java class.

---
+++ Saving Stacks/src/application/view/GoalController.java
@@ -0,0 +1,26 @@
+package application.controller;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent;
+
+public class GoalController implements EventHandler<ActionEvent>{
+
+	@Override
+	public void handle(ActionEvent event) {
+		
+		
+	}
+	
+	public void mouseClicked(MouseEvent e)
+	{
+	
+
+	}
+	
+	public void mousePressed()
+	{
+		System.out.print("ok");
+	}
+
+}

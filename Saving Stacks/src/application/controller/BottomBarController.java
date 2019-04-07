ab417370b6d0cbffe23ad03bfec8a53481fd381f
Gabriel Morales <da.gabrielmora@gmail.com>
2019-04-07 18:07:13 -0500

    Improved settings UI. New Dark color scheme

--- Saving Stacks/src/application/controller/BottomBarController.java
+++ Saving Stacks/src/application/controller/BottomBarController.java
@@ -25,12 +25,13 @@ import javafx.scene.text.Font;
 /**
  * @author Gabriel Morales
  * 
+ * <h1>
  * BottomBarController class is intended to provided
  * the functionality for the navigation central
  * to the application. This will contain 
  * all the logic pertaining to the bottom
  * bar as it persists through the views.
- * 
+ * </h1>
  */
 
 public class BottomBarController {
--- Saving Stacks/src/application/controller/SettingController.java
+++ Saving Stacks/src/application/controller/SettingController.java
@@ -12,8 +12,10 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.Separator;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.util.Duration;
 
@@ -25,11 +27,16 @@ public class SettingController implements Initializable, EventHandler<ActionEven
 	@FXML
 	AnchorPane settingAnchor;
 	@FXML
-	Label title, security, customization, passPrompt, accentColor;
+	Label title, security, customization;
 	@FXML
-	RadioButton passwordRadio, lightMode, darkMode;
+	RadioButton passwordRadio;
 	@FXML
 	TextField password;
+	@FXML
+	Pane accents;
+	@FXML
+	Button darkMode, lightMode;
+
 	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
@@ -39,17 +46,18 @@ public class SettingController implements Initializable, EventHandler<ActionEven
 		settingAnchor.backgroundProperty().bind(bottomBar.getBackingPane().backgroundProperty());
 		security.textFillProperty().bind(title.textFillProperty());
 		customization.textFillProperty().bind(title.textFillProperty());
-		passPrompt.textFillProperty().bind(title.textFillProperty());
-		accentColor.textFillProperty().bind(title.textFillProperty());
 		passwordRadio.textFillProperty().bind(title.textFillProperty());
+		accents.backgroundProperty().bind(password.backgroundProperty());
+		lightMode.backgroundProperty().bind(settingAnchor.backgroundProperty());
 		lightMode.textFillProperty().bind(title.textFillProperty());
-		darkMode.textFillProperty().bind(title.textFillProperty());
+		
 		
 		for(Button b : bottomBar.getBarButtons())
 		{
 			b.backgroundProperty().bind(bottomBar.getBackingPane().backgroundProperty());
 			b.textFillProperty().bind(title.textFillProperty());
 		}
+		
 	}
 
 	@Override
@@ -62,12 +70,16 @@ public class SettingController implements Initializable, EventHandler<ActionEven
 	
 	public void darkHandle(ActionEvent darkEvent)
 	{
-		ColorTransition bottomBarTransition = new ColorTransition(Duration.millis(500), bottomBar.getBackingPane(), Color.web("ffffff"), Color.web("424242"), "-fx-background-color: ");
+		ColorTransition bottomBarTransition = new ColorTransition(Duration.millis(500), bottomBar.getBackingPane(), Color.web("ffffff"), Color.web("33333d"), "-fx-background-color: ");
 		ColorTransition textColor = new ColorTransition(Duration.millis(500), title, Color.web("000000"), Color.web("ffffff"), "-fx-text-fill: ");
+		ColorTransition passwordColor = new ColorTransition(Duration.millis(500), password, Color.web("F5F5F5"), Color.web("25282f"), "-fx-text-fill: white; -fx-background-radius: 30; -fx-background-color: ");
 		
-		ParallelTransition pt = new ParallelTransition(bottomBarTransition, textColor);
 		
 		
+		ParallelTransition pt = new ParallelTransition(bottomBarTransition, textColor, passwordColor);
+		
+		darkMode.setDisable(true);
+		lightMode.setDisable(false);
 		
 		pt.play();
 	}
@@ -75,7 +87,18 @@ public class SettingController implements Initializable, EventHandler<ActionEven
 	public void lightHandle(ActionEvent darkEvent)
 	{
 		
+		ColorTransition bottomBarTransition = new ColorTransition(Duration.millis(500), bottomBar.getBackingPane(), Color.web("33333d"), Color.web("ffffff"), "-fx-background-color: ");
+		ColorTransition textColor = new ColorTransition(Duration.millis(500), title, Color.web("ffffff"), Color.web("000000"), "-fx-text-fill: ");
+		ColorTransition passwordColor = new ColorTransition(Duration.millis(500), password, Color.web("25282f"), Color.web("F5F5F5"), "-fx-background-radius: 30; -fx-background-color: ");
 		
+		
+		
+		ParallelTransition pt = new ParallelTransition(bottomBarTransition, textColor, passwordColor);
+
+		darkMode.setDisable(false);
+		lightMode.setDisable(true);
+		
+		pt.play();
 	}
 
 }

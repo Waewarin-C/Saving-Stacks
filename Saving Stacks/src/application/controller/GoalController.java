0400976ccdb192a07e6327cce481050897f0d341
chels <chels@192.168.0.4>
2019-04-17 10:48:42 -0500

    Updated GUI organization

--- Saving Stacks/src/application/controller/GoalController.java
+++ Saving Stacks/src/application/controller/GoalController.java
@@ -4,12 +4,8 @@ import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.ResourceBundle;
-
 import application.Main;
-import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -17,17 +13,14 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
-import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.SVGPath;
-import javafx.scene.text.Font;
 
 public class GoalController implements EventHandler<ActionEvent>, Initializable{
 	
@@ -69,8 +62,12 @@ public class GoalController implements EventHandler<ActionEvent>, Initializable{
 				generateRowIcons(i, null);
 			}*/
 		else
-			generateRowIcons(0, null);
-		
+		{
+			setGUIRowVisible( 0 );
+			addUnlockIcon( 0 );
+			addRemoveIcon( 0 );
+			addAddIcon( 0 );
+		}
 
 
 		
@@ -90,7 +87,10 @@ public class GoalController implements EventHandler<ActionEvent>, Initializable{
 		{
 			n.setVisible(false);
 			setGUIRowVisible( row + 1 );
-			generateRowIcons( row + 1, null );
+			addUnlockIcon( row +1 );
+			addRemoveIcon( row + 1 );
+			if( row < MAX_ROWS - 2 )
+				addAddIcon( row + 1 );
 		}
 		else if( id.equals("delete"))
 		{
@@ -132,22 +132,20 @@ public class GoalController implements EventHandler<ActionEvent>, Initializable{
 		}
 	}
 	
-	public void generateRowIcons( int row, File f )
+	public void addUnlockIcon( int row )
 	{
-	
-		//ObservableList<Node> goalGrid = this.getGoalGrid().getChildren();
-		
-		/*
 		Button unlock = new Button();
 		SVGPath unlockPath = new SVGPath();
 		unlockPath.setContent("M18 8h-1V6c0-2.76-2.24-5-5-5S7 3.24 7 6h2c0-1.66 1.34-3 3-3s3 1.34 3 3v2H6c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V10c0-1.1-.9-2-2-2zm0 12H6V10h12v10zm-6-3c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2z");
 		unlock.setGraphic(unlockPath);
 		unlock.setId("unlock");
-		unlock.setOnAction(this)
+		unlock.setOnAction(this);
 		unlock.setBackground(new Background(new BackgroundFill(Color.TRANSPARENT, null, null)));
 		goalGrid.add(unlock, 3, row);
-		 */
-		
+	}
+	
+	public void addLockIcon( int row )
+	{
 		Button lock = new Button();
 		SVGPath lockPath = new SVGPath();
 		lockPath.setContent("M18 8h-1V6c0-2.76-2.24-5-5-5S7 3.24 7 6v2H6c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V10c0-1.1-.9-2-2-2zM9 6c0-1.66 1.34-3 3-3s3 1.34 3 3v2H9V6zm9 14H6V10h12v10zm-6-3c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2z");
@@ -156,8 +154,10 @@ public class GoalController implements EventHandler<ActionEvent>, Initializable{
 		lock.setOnAction(this);
 		lock.setBackground(new Background(new BackgroundFill(Color.TRANSPARENT, null, null)));	
 		goalGrid.add(lock, 3, row);
+	}
 	
-		
+	public void addRemoveIcon( int row )
+	{
 		Button remove = new Button();
 		SVGPath removePath = new SVGPath();
 		removePath.setContent("M7 11v2h10v-2H7zm5-9C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z");
@@ -165,21 +165,22 @@ public class GoalController implements EventHandler<ActionEvent>, Initializable{
 		remove.setId("remove");
 		remove.setOnAction(this);
 		remove.setBackground(new Background(new BackgroundFill(Color.TRANSPARENT, null, null)));		
-		goalGrid.add(removePath, 4, row);
-		
-		if( row < MAX_ROWS - 1)
-		{
-			Button add = new Button();
-			SVGPath addPath = new SVGPath();
-			addPath.setContent("M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z");
-			add.setGraphic(addPath);
-			add.setId("add");
-			add.setOnAction(this);
-			add.setBackground(new Background(new BackgroundFill(Color.TRANSPARENT, null, null)));		
-			goalGrid.add(add, 5, row);
-		}
+		goalGrid.add(removePath, 4, row);	
+	}
+	
+	public void addAddIcon( int row )
+	{
+		Button add = new Button();
+		SVGPath addPath = new SVGPath();
+		addPath.setContent("M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z");
+		add.setGraphic(addPath);
+		add.setId("add");
+		add.setOnAction(this);
+		add.setBackground(new Background(new BackgroundFill(Color.TRANSPARENT, null, null)));		
+		goalGrid.add(add, 5, row);
 		
 	}
+
 }
 
 

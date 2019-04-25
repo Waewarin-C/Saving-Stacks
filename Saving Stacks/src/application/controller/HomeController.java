0a87902de374640091be373b75adb1bd21f050ef
Waewarin Chindarassami <w.chindarassami@gmail.com>
2019-04-25 14:49:20 -0500

    Added pie chart to Home page, right now it is a rough draft just based on what is entered in the Goals page

--- Saving Stacks/src/application/controller/HomeController.java
+++ Saving Stacks/src/application/controller/HomeController.java
@@ -1,11 +1,20 @@
 package application.controller;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.ResourceBundle;
 
 import application.Main;
+import application.model.Goal;
+import application.model.GoalSet;
+import application.model.Home;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.PieChart;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
@@ -14,6 +23,10 @@ import javafx.scene.layout.Pane;
 public class HomeController implements Initializable {
 
 	private static final String controllerID = "HOME";
+	
+	private GoalSet goals;
+	private HashMap<Integer, Goal> goalMap;
+	private Home home;
 	
 	@FXML
 	AnchorPane homeAnchor;
@@ -24,6 +37,12 @@ public class HomeController implements Initializable {
 	@FXML
 	Label moneyPrompt, spendingPrompt, goalPrompt, budget;
 	
+	@FXML
+	PieChart spendingChart;
+	
+	@FXML
+	BarChart goalGraph;
+	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		
@@ -32,8 +51,14 @@ public class HomeController implements Initializable {
 		
 		Double budgetRemaining = Double.valueOf(Main.settings.getValueWithProperty("monthly_budget"));
 		
+		goals = new GoalSet();
+		goals.loadGoals("data/goals.csv");
 		
+		home = new Home();
 		
+		ArrayList<PieChart.Data> spending = home.retrieveData(goals);
+		ObservableList<PieChart.Data> data = FXCollections.observableList(spending);
+		spendingChart.setData(data);
 		
 		goalPrompt.textFillProperty().bind(moneyPrompt.textFillProperty());
 		spendingPrompt.textFillProperty().bind(moneyPrompt.textFillProperty());

7b079fecc5365250977e855d6b88d90e9527430e
Gabriel Morales <da.gabrielmora@gmail.com>
2019-04-25 16:17:24 -0500

    Upload pages now work

--- Saving Stacks/src/application/controller/UploadController.java
+++ Saving Stacks/src/application/controller/UploadController.java
@@ -45,6 +45,15 @@ public class UploadController implements EventHandler<ActionEvent>, Initializabl
 	private HashMap<String, Goal> goalMap;
 	private GoalSet goals;
 	
+	/**
+	 *  NOTE: the following lists are for removal from GridPane
+	 */
+	private ArrayList<ChoiceBox<String>> choiceBoxes;
+	private ArrayList<Label> labels;
+	private ArrayList<TextField> textFields;
+	
+	
+	
 	private static final String BACKGROUND_COLOR_STYLE = "-fx-background-color: #33333d";
 	private static final String controllerID = "UPLOAD";
 	private static final int MAX_ROWS = 11;
@@ -85,9 +94,9 @@ public class UploadController implements EventHandler<ActionEvent>, Initializabl
 		for (int i = 0; i < transactions.size(); i++, cnt++)
 		{
 			
+			arrayIndicator = i;
 			if (cnt == MAX_ROWS)
 			{
-				arrayIndicator = i;
 				break;
 			}
 			
@@ -135,9 +144,16 @@ public class UploadController implements EventHandler<ActionEvent>, Initializabl
 			GridPane.setHalignment(cb,  HPos.CENTER);
 			
 			gridView.add(l, 0, i);
+			labels.add(l);
+			
 			gridView.add(tf, 1, i);
+			textFields.add(tf);
+			
 			gridView.add(price, 2, i);
+			labels.add(price);
+			
 			gridView.add(cb, 3, i);
+			choiceBoxes.add(cb);
 			
 		}
 		
@@ -149,8 +165,87 @@ public class UploadController implements EventHandler<ActionEvent>, Initializabl
 	public void getNextItems(ActionEvent arg0)
 	{
 		
+		
+		
+		gridView.getChildren().removeAll(choiceBoxes);
+		gridView.getChildren().removeAll(labels);
+		gridView.getChildren().removeAll(textFields);
+		
+		
+		choiceBoxes.clear();
+		labels.clear();
+		textFields.clear();
+		
+		int cnt = 0;
+		for (int i = arrayIndicator; i < transactions.size(); i++, cnt++)
+		{
+			arrayIndicator = i;
+			if (cnt == MAX_ROWS)
+			{
+				break;
+			}
+			
+			Label l = new Label(transactions.get(i).getTransDate());
+			
+			DecimalFormat df = new DecimalFormat("#.00");
+	        String strAmt = df.format(transactions.get(i).getAmount());
+			Label price = new Label(strAmt);
+			
+			TextField tf = new TextField();
+			tf.setText(transactions.get(i).getName());
+			
+			ChoiceBox<String> cb = new ChoiceBox<>();
+			cb.getItems().addAll(goalNames);
+			
+			
+			
+			if (Main.settings.getBooleanValueWithProperty("is_dark_mode_enabled"))
+			{
+				cb.setStyle("");
+				cb.getStylesheets().add(getClass().getResource("../view/choice_dark.css").toExternalForm());
+				
+				tf.setStyle("-fx-background-color: #25282f; -fx-background-radius: 30; -fx-text-fill: white");
+				
+				l.setTextFill(Color.WHITE);
+				price.setTextFill(Color.WHITE);
+				
+			}
+			else
+			{
+				cb.setStyle("");
+				cb.setStyle("-fx-background-color: #F5F5F5; -fx-background-radius: 30; -fx-mark-color: black");
+				
+				tf.setStyle("-fx-background-color: #F5F5F5; -fx-background-radius: 30; -fx-text-fill: black");
+				
+				l.setTextFill(Color.BLACK);
+				price.setTextFill(Color.BLACK);
+				
+			}
+			
+			
+			GridPane.setHalignment(l,  HPos.CENTER);
+			GridPane.setHalignment(tf,  HPos.CENTER);
+			GridPane.setHalignment(price,  HPos.CENTER);
+			GridPane.setHalignment(cb,  HPos.CENTER);
+			
+			gridView.add(l, 0, cnt);
+			labels.add(l);
+			
+			gridView.add(tf, 1, cnt);
+			textFields.add(tf);
+			
+			gridView.add(price, 2, cnt);
+			labels.add(price);
+			
+			gridView.add(cb, 3, cnt);
+			choiceBoxes.add(cb);
+			
+		
+		}
 
 		
+		if (arrayIndicator == transactions.size() - 1)
+			continueButton.setVisible(false);
 		
 	}
 
@@ -160,6 +255,14 @@ public class UploadController implements EventHandler<ActionEvent>, Initializabl
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		
 		gridView.toBack();
+		
+		arrayIndicator = 0;
+		
+		choiceBoxes = new ArrayList<>();
+		labels = new ArrayList<>();
+		textFields = new ArrayList<>();
+		
+		
 		
 		if (Main.settings.getBooleanValueWithProperty("is_dark_mode_enabled"))
 		{

948cf05184fc6ea4383746f23c543f4162c6110f
Gabriel Morales <da.gabrielmora@gmail.com>
2019-04-25 21:30:04 -0500

    Zoom in feature

--- Saving Stacks/src/application/controller/HomeController.java
+++ Saving Stacks/src/application/controller/HomeController.java
@@ -9,12 +9,15 @@ import application.Main;
 import application.model.Goal;
 import application.model.GoalSet;
 import application.model.Home;
+import javafx.animation.FadeTransition;
+import javafx.animation.ParallelTransition;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.SnapshotParameters;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
@@ -23,8 +26,18 @@ import javafx.scene.chart.XYChart;
 import javafx.scene.chart.XYChart.Data;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.effect.GaussianBlur;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.WritableImage;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.util.Duration;
 
 
 public class HomeController implements EventHandler<ActionEvent>, Initializable {
@@ -51,6 +64,238 @@ public class HomeController implements EventHandler<ActionEvent>, Initializable 
 	
 	@FXML
 	Button weeklyButton, monthlyButton, yearlyButton;
+	
+	
+	public void expandGraphToView(ActionEvent arg0)
+	{
+		
+		
+		
+		Button b = new Button("Done");
+		b.setPrefHeight(37);
+		b.setPrefWidth(120);
+		b.setLayoutX(545);
+		b.setLayoutY(45);
+		b.setFont(new Font("Segoe UI", 20));
+		b.setStyle("-fx-background-color: black; -fx-text-fill: white; -fx-background-radius: 30");
+		
+		
+		DropShadow ds = new DropShadow();
+		ds.setWidth(21.0);
+		ds.setHeight(21.0);
+		ds.setRadius(15);
+		ds.setSpread(0.2);
+		ds.setColor(Color.color(0, 0, 0, .50));
+		
+		Pane blurPane = new Pane();
+		blurPane.setPrefHeight(584);
+		blurPane.setPrefWidth(584);
+		
+		blurPane.setLayoutX(101);
+		blurPane.setLayoutY(108);
+		
+		blurPane.setEffect(ds);
+		
+		GaussianBlur blur = new GaussianBlur(38.0);
+		
+		WritableImage image = homeAnchor.snapshot(new SnapshotParameters(), null);
+		
+		
+		ImageView img = new ImageView(image);
+		img.setLayoutX(-30);
+		img.setLayoutY(0);
+		img.setEffect(blur);
+		img.setFitHeight(860);
+		img.setFitWidth(860);
+		
+		if (Main.settings.getBooleanValueWithProperty("is_dark_mode_enabled"))
+		{
+			blurPane.setBackground(new Background(new BackgroundFill(Color.web("#25282f"), new CornerRadii(30), null)));
+		}
+		else
+		{
+			blurPane.setBackground(new Background(new BackgroundFill(Color.WHITE, new CornerRadii(30), null)));
+		}
+		
+		
+		homeAnchor.getChildren().add(img);
+		homeAnchor.getChildren().add(blurPane);
+		homeAnchor.getChildren().add(b);
+		
+		img.toFront();
+		blurPane.toFront();
+		b.toFront();
+		blurPane.getChildren().add(goalGraph);
+		goalGraph.setLayoutX(0);
+		goalGraph.setLayoutY(0);
+		goalGraph.setPrefHeight(574);
+		goalGraph.setPrefWidth(584);
+		
+		FadeTransition imgFT = new FadeTransition(Duration.millis(200), img);
+		imgFT.setFromValue(0.0);
+		imgFT.setToValue(1.0);
+		
+		FadeTransition blurPaneFT = new FadeTransition(Duration.millis(200), blurPane);
+		blurPaneFT.setFromValue(0.0);
+		blurPaneFT.setToValue(1.0);
+		
+		FadeTransition buttonFT = new FadeTransition(Duration.millis(200), b);
+		buttonFT.setFromValue(0.0);
+		buttonFT.setToValue(1.0);
+		
+		ParallelTransition pt = new ParallelTransition(imgFT, blurPaneFT, buttonFT);
+		
+		
+		
+		pt.play();
+		
+		
+		
+		b.addEventHandler(ActionEvent.ACTION, new EventHandler<ActionEvent>() {
+
+			@Override
+			 public void handle(ActionEvent arg0) {
+				
+				blurPane.getChildren().remove(spendingChart);
+				
+				goalGraph.setLayoutX(15);
+				goalGraph.setLayoutY(14);
+				goalGraph.setPrefHeight(310);
+				goalGraph.setPrefWidth(350);
+				goalPane.getChildren().add(goalGraph);
+				
+				
+				
+				
+				homeAnchor.getChildren().remove(img);
+				homeAnchor.getChildren().remove(blurPane);
+				homeAnchor.getChildren().remove(b);
+				
+				
+			
+			}
+			
+		});
+		
+		
+	}
+	
+	
+	public void expandPieChartToView(ActionEvent arg0)
+	{
+		
+		
+		
+		Button b = new Button("Done");
+		b.setPrefHeight(37);
+		b.setPrefWidth(120);
+		b.setLayoutX(545);
+		b.setLayoutY(45);
+		b.setFont(new Font("Segoe UI", 20));
+		b.setStyle("-fx-background-color: black; -fx-text-fill: white; -fx-background-radius: 30");
+		
+		
+		DropShadow ds = new DropShadow();
+		ds.setWidth(21.0);
+		ds.setHeight(21.0);
+		ds.setRadius(15);
+		ds.setSpread(0.2);
+		ds.setColor(Color.color(0, 0, 0, .50));
+		
+		Pane blurPane = new Pane();
+		blurPane.setPrefHeight(584);
+		blurPane.setPrefWidth(584);
+		
+		blurPane.setLayoutX(101);
+		blurPane.setLayoutY(108);
+		
+		blurPane.setEffect(ds);
+		
+		GaussianBlur blur = new GaussianBlur(38.0);
+		
+		WritableImage image = homeAnchor.snapshot(new SnapshotParameters(), null);
+		
+		
+		ImageView img = new ImageView(image);
+		img.setLayoutX(-30);
+		img.setLayoutY(0);
+		img.setEffect(blur);
+		img.setFitHeight(860);
+		img.setFitWidth(860);
+		
+		if (Main.settings.getBooleanValueWithProperty("is_dark_mode_enabled"))
+		{
+			blurPane.setBackground(new Background(new BackgroundFill(Color.web("#25282f"), new CornerRadii(30), null)));
+		}
+		else
+		{
+			blurPane.setBackground(new Background(new BackgroundFill(Color.WHITE, new CornerRadii(30), null)));
+		}
+		
+		
+		homeAnchor.getChildren().add(img);
+		homeAnchor.getChildren().add(blurPane);
+		homeAnchor.getChildren().add(b);
+		
+		img.toFront();
+		blurPane.toFront();
+		b.toFront();
+		blurPane.getChildren().add(spendingChart);
+		spendingChart.setLayoutX(0);
+		spendingChart.setLayoutY(0);
+		spendingChart.setPrefHeight(574);
+		spendingChart.setPrefWidth(584);
+		
+		FadeTransition imgFT = new FadeTransition(Duration.millis(200), img);
+		imgFT.setFromValue(0.0);
+		imgFT.setToValue(1.0);
+		
+		FadeTransition blurPaneFT = new FadeTransition(Duration.millis(200), blurPane);
+		blurPaneFT.setFromValue(0.0);
+		blurPaneFT.setToValue(1.0);
+		
+		FadeTransition buttonFT = new FadeTransition(Duration.millis(200), b);
+		buttonFT.setFromValue(0.0);
+		buttonFT.setToValue(1.0);
+		
+		ParallelTransition pt = new ParallelTransition(imgFT, blurPaneFT, buttonFT);
+		
+		
+		
+		pt.play();
+		
+		
+		
+		b.addEventHandler(ActionEvent.ACTION, new EventHandler<ActionEvent>() {
+
+			@Override
+			 public void handle(ActionEvent arg0) {
+				
+				blurPane.getChildren().remove(spendingChart);
+				
+				spendingChart.setLayoutX(15);
+				spendingChart.setLayoutY(14);
+				spendingChart.setPrefHeight(310);
+				spendingChart.setPrefWidth(350);
+				spendPane.getChildren().add(spendingChart);
+				
+				
+				
+				
+				homeAnchor.getChildren().remove(img);
+				homeAnchor.getChildren().remove(blurPane);
+				homeAnchor.getChildren().remove(b);
+				
+				
+			
+			}
+			
+		});
+		
+		
+	}
+	
+	
 	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
@@ -124,6 +369,10 @@ public class HomeController implements EventHandler<ActionEvent>, Initializable 
 			homeAnchor.setStyle("-fx-background-color: #33333d");
 			moneyPrompt.setStyle("-fx-text-fill: white");
 			switchPane.setStyle("-fx-background-color: #25282f; -fx-background-radius: 30");
+			
+			spendingChart.getStylesheets().add(getClass().getResource("../view/chart_dark.css").toExternalForm());
+			goalGraph.getXAxis().setTickLabelFill(Color.WHITE);
+			goalGraph.getYAxis().setTickLabelFill(Color.WHITE);
 			
 			budget.setStyle("-fx-text-fill: #60ad5e");
 		}
